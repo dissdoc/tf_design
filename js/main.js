8fc124d190777f475ce02fec508b779d5e4a76a2
Kirill Mukhov <dissdoc@gmail.com>
2014-08-11 20:55:48 +0400

    close #2 add color track

--- js/main.js
+++ js/main.js
@@ -95,27 +95,27 @@ $(function() {
 $(function() {
     $('.chart-open').easyPieChart({
         barColor: '#f89406',
-        trackColor: '#f1f1f1',
+        trackColor: '#f8e4c6',
         size: 105
     });
 });
 $(function() {
     $('.chart-start').easyPieChart({
         barColor: '#3a87ad',
-        trackColor: '#f1f1f1',
+        trackColor: '#dee7ec',
         size: 105
     });
 });
 $(function() {
     $('.chart-resolved').easyPieChart({
         barColor: '#080',
-        trackColor: '#f1f1f1',
+        trackColor: '#ddf6dd',
         size: 105
     });
 });
 $(function() {
     $('.chart-closed').easyPieChart({
-        barColor: '#ccc',
+        barColor: '#bbb',
         trackColor: '#f1f1f1',
         size: 105
     });
